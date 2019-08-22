d0536646525b2329096e3d967dea0bbb1b9ddb7c
mrprince <mrprince@qq.com>
2019-08-22 14:17:38 +0800

    react + typescript + pact

---
+++ src/person/person.service.test.tsx
@@ -0,0 +1,63 @@
+import path from "path";
+import PersonService from './person.service';
+import { Pact, Interaction, Matchers } from "@pact-foundation/pact"
+import Person from './person';
+
+const provider = new Pact({
+  cors: true,
+  port: 8888,
+  log: path.resolve(process.cwd(), 'logs', 'pact.log'),
+  dir: path.resolve(process.cwd(), 'pacts'),
+  spec: 2,
+  pactfileWriteMode: 'update',
+  consumer: 'person-react-consumer',
+  provider: 'person-provider',
+  host: '127.0.0.1'
+});
+
+
+describe('PersonService API', () => {
+
+  const personService = new PersonService('http://localhost', 8888);
+
+  beforeAll(() => provider.setup());
+
+  afterAll(() => provider.verify() && provider.finalize());
+
+  describe("get person 0", () => {
+
+    beforeAll(() => {
+
+      const interaction = new Interaction()
+        .given("Person 0 exists")
+        .uponReceiving("A request to /api/person/0")
+        .withRequest({
+          method: "GET",
+          path: "/api/person/0",
+          headers: {
+            Accept: "application/json",
+          },
+        })
+        .willRespondWith({
+          status: 200,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: new Person(0, 'Michael Johansen', 50),
+        });
+
+      return provider.addInteraction(interaction)
+    });
+
+    it('sends a request according to contract', (done) => {
+      personService.getPerson(0)
+        .then((person: Person) => {
+          expect(person.name).toEqual('Michael Johansen');
+          done();
+        });
+
+    });
+
+  })
+
+});   
---
+++ src/person/person.service.tsx
@@ -0,0 +1,32 @@
+import Person from "./person";
+import axios from 'axios';
+
+class PersonService {
+
+    baseUrl:string;
+
+    port:number;
+
+    constructor(baseUrl:string, port:number){
+        this.baseUrl = baseUrl;
+        this.port = port;
+    }
+
+
+    getPerson(id:number): Promise<Person>  {
+        if (id == null) {
+            throw new Error("id must not be null!");
+        }
+        return axios.request({
+            method: 'GET',
+            url: `/api/person/${id}`,
+            baseURL: `${this.baseUrl}:${this.port}`,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => response.data as Person) as Promise<Person>;
+    };
+
+}
+
+export default PersonService;
